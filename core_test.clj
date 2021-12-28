0df3357eeea30ab5bf61fb310cb1984b5c7e6a16
Lin1Maoran <93834837+Lin1Maoran@users.noreply.github.com>
2021-12-28 17:31:29 +0800

    Add files via upload

---
+++ core_test.clj
@@ -0,0 +1,8 @@
+(ns financial-signaling-game.core-test
+  #_{:clj-kondo/ignore [:refer-all]}
+  (:require [clojure.test :refer :all]
+            [financial-signaling-game.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ intro.md
@@ -0,0 +1,3 @@
+# Introduction to financial-signaling-game
+;; Welcome to the world of financial signaling game!!!!
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
